d6e77fb48fc8ed4a9fb94b3ee64bd9618e4dccb6
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-20 13:24:32 +0100

    Flightresource, now runs in parallel, urls still missing Datafacade, methods still to be done

--- SysProjectBackend/src/main/java/rest/FlightsResource.java
+++ SysProjectBackend/src/main/java/rest/FlightsResource.java
@@ -6,7 +6,13 @@ import com.google.gson.JsonArray;
 import facade.DataFacade;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
@@ -25,8 +31,8 @@ public class FlightsResource {
     @Context
     private UriInfo context;
     private Gson gson;
-    private static String DATBOI$URL = "";
-    private static String MIXURL = "";
+    private static final String DATBOI$URL = "";
+    private static final String MIXURL = "";
 
     public FlightsResource() {
         gson = new GsonBuilder().setPrettyPrinting().create();
@@ -37,73 +43,80 @@ public class FlightsResource {
     public String getFlights() {
         return gson.toJson(DataFacade.getAllFlights());
     }
-    
+
     @GET
     @Path("all")
     @Produces(MediaType.APPLICATION_JSON)
     public String getAllFlights() {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        List<Future<String>> data = new ArrayList<>();
+        data.add(executor.submit(() -> {
+            return getFlights();
+        }));
+        data.add(executor.submit(() -> {
+            return getDatboi$Data();
+        }));
+        data.add(executor.submit(() -> {
+            return getMixData();
+        }));
         JsonArray ja = new JsonArray();
-        ja.add(getFlights());
-        ja.add(getDatboi$Data());
-        ja.add(getMixData());
+        for (Future<String> fut : data) {
+            try {
+                ja.add(fut.get());
+            } catch (InterruptedException | ExecutionException ex) {
+                Logger.getLogger(FlightsResource.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         return ja.getAsString();
     }
 
     @POST
     @Path("new")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void postFlights(String content){
+    public void postFlights(String content) {
         DataFacade.addNewFlight();
     }
-    
+
     @PUT
     @Path("edit")
     @Consumes(MediaType.APPLICATION_JSON)
     public void putFlights(String content) {
         DataFacade.editFlight();
     }
-    
-    public static String getDatboi$Data()
-    {
+
+    public static String getDatboi$Data() {
         String jsonStr = null;
-        try
-        {
+        try {
             URL url = new URL(DATBOI$URL);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Accept", "application/json;charset=UTF-8");
             con.setRequestProperty("User-Agent", "server");
             Scanner scan = new Scanner(con.getInputStream());
-            if (scan.hasNext())
-            {
+            if (scan.hasNext()) {
                 jsonStr = scan.nextLine();
             }
             scan.close();
-        } catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Logger.getLogger(FlightsResource.class.getName()).log(Level.SEVERE, null, ex);
         }
         return jsonStr;
     }
-    
-    public static String getMixData()
-    {
+
+    public static String getMixData() {
         String jsonStr = null;
-        try
-        {
+        try {
             URL url = new URL(MIXURL);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Accept", "application/json;charset=UTF-8");
             con.setRequestProperty("User-Agent", "server");
             Scanner scan = new Scanner(con.getInputStream());
-            if (scan.hasNext())
-            {
+            if (scan.hasNext()) {
                 jsonStr = scan.nextLine();
             }
             scan.close();
-        } catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Logger.getLogger(FlightsResource.class.getName()).log(Level.SEVERE, null, ex);
         }
         return jsonStr;
