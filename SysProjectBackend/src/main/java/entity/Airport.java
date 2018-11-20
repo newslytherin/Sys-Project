de8b0bf1a18f44624d005f4a6d171f6f9daf8e44
jack-borg <jacob.fam.borg@gmail.com>
2018-11-20 22:35:38 +0100

    entity classes

--- SysProjectBackend/src/main/java/entity/Airport.java
+++ SysProjectBackend/src/main/java/entity/Airport.java
@@ -1,6 +1,67 @@
 package entity;
 
-class Airport 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Airport implements Serializable
 {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String country;
+    private String countryCode;
+    private String city;
+
+    public Airport()
+    {
+    }
+
+    public Airport(String country, String countryCode, String city)
+    {
+        this.country = country;
+        this.countryCode = countryCode;
+        this.city = city;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getCountryCode()
+    {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode)
+    {
+        this.countryCode = countryCode;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
 
 }
--- SysProjectBackend/src/main/java/entity/Flight.java
+++ SysProjectBackend/src/main/java/entity/Flight.java
@@ -26,17 +26,150 @@ public class Flight implements Serializable
     private String airplane;
     private String model;
     private int capacity;
-    
-    
 
+    public Flight()
+    {
+    }
+
+    public Flight(String airline, Airport departure, Airport destination, String date, String depTime, String arrTime, int duration, int price, int cancelInsurance, String airplane, String model, int capacity)
+    {
+        this.airline = airline;
+        this.departure = departure;
+        this.destination = destination;
+        this.date = date;
+        this.depTime = depTime;
+        this.arrTime = arrTime;
+        this.duration = duration;
+        this.price = price;
+        this.cancelInsurance = cancelInsurance;
+        this.airplane = airplane;
+        this.model = model;
+        this.capacity = capacity;
+    }
+
+    public String getAirline()
+    {
+        return airline;
+    }
+
+    public void setAirline(String airline)
+    {
+        this.airline = airline;
+    }
+
+    public Airport getDeparture()
+    {
+        return departure;
+    }
+
+    public void setDeparture(Airport departure)
+    {
+        this.departure = departure;
+    }
+
+    public Airport getDestination()
+    {
+        return destination;
+    }
+
+    public void setDestination(Airport destination)
+    {
+        this.destination = destination;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public String getDepTime()
+    {
+        return depTime;
+    }
+
+    public void setDepTime(String depTime)
+    {
+        this.depTime = depTime;
+    }
+
+    public String getArrTime()
+    {
+        return arrTime;
+    }
+
+    public void setArrTime(String arrTime)
+    {
+        this.arrTime = arrTime;
+    }
+
+    public int getDuration()
+    {
+        return duration;
+    }
+
+    public void setDuration(int duration)
+    {
+        this.duration = duration;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(int price)
+    {
+        this.price = price;
+    }
+
+    public int getCancelInsurance()
+    {
+        return cancelInsurance;
+    }
+
+    public void setCancelInsurance(int cancelInsurance)
+    {
+        this.cancelInsurance = cancelInsurance;
+    }
+
+    public String getAirplane()
+    {
+        return airplane;
+    }
+
+    public void setAirplane(String airplane)
+    {
+        this.airplane = airplane;
+    }
+
+    public String getModel()
+    {
+        return model;
+    }
+
+    public void setModel(String model)
+    {
+        this.model = model;
+    }
+
+    public int getCapacity()
+    {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity)
+    {
+        this.capacity = capacity;
+    }
+    
     public Long getId()
     {
         return id;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
     }
     
     @Override
