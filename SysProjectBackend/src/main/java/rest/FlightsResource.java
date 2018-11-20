a01b75271e2ab1a902bfc4722793c34fa05a44f9
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-20 12:51:17 +0100

    Flightresource, urls missing Datafacade, methods still to be done

--- SysProjectBackend/src/main/java/rest/FlightsResource.java
+++ SysProjectBackend/src/main/java/rest/FlightsResource.java
@@ -3,7 +3,6 @@ package rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import facade.DataFacade;
 import java.net.HttpURLConnection;
 import java.net.URL;
