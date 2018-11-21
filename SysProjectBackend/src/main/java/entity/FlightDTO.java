162a7d835a38cbf2594926ec47df36a66cab7697
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-21 12:43:08 +0100

    endpoint for get is working

--- SysProjectBackend/src/main/java/entity/FlightDTO.java
+++ SysProjectBackend/src/main/java/entity/FlightDTO.java
@@ -27,4 +27,93 @@ public class FlightDTO {
         model = f.getModel();
         capacity = f.getCapacity();
     }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepTime() {
+        return depTime;
+    }
+
+    public void setDepTime(String depTime) {
+        this.depTime = depTime;
+    }
+
+    public String getArrTime() {
+        return arrTime;
+    }
+
+    public void setArrTime(String arrTime) {
+        this.arrTime = arrTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCancelInsurance() {
+        return cancelInsurance;
+    }
+
+    public void setCancelInsurance(int cancelInsurance) {
+        this.cancelInsurance = cancelInsurance;
+    }
+
+    public String getAirplane() {
+        return airplane;
+    }
+
+    public void setAirplane(String airplane) {
+        this.airplane = airplane;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    
 }
