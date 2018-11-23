c2d746defeeeaa9a860abcb5cc47f5f7145fc03a
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-23 09:27:25 +0100

    testing

--- SQLdata/GenerateData.js
+++ SQLdata/GenerateData.js
@@ -40,6 +40,40 @@ const airports = [
     { id: 18, city: 'Zambezi', country: 'Zambia', code: 'ZM', name: 'BBZ' },
     { id: 19, city: 'Vancouver', country: 'Canada', code: 'CA', name: 'CXH' }
 ]
+
+var airports2 = [    
+    {city:"London",code:"LHR",country:"England"},
+    {city:"Paris",code:"CDG",country:"Frankrig"},
+    {city:"Amsterdam",code:"AMS",country:"Holland"},
+    {city:"Frankfurt",code:"FRA",country:"Tyskland"},
+    {city:"Istanbul",code:"IST",country:"Tyrkiet"},
+    {city:"Madrid",code:"MAD",country:"Spanien"},
+    {city:"Barcelona",code:"BCN",country:"Spanien"},
+    {city:"London",code:"LGW",country:"England"},
+    {city:"München",code:"MUC",country:"Tyskland"},
+    {city:"Rom",code:"FCO",country:"Italien"},
+    {city:"Moskva",code:"SVO",country:"Rusland"},
+    {city:"Paris",code:"ORY",country:"Frankrig"},
+    {city:"Moskva",code:"DME",country:"Rusland"},
+    {city:"Dublin",code:"DUB",country:"Irland"},
+    {city:"Zürich",code:"ZRH",country:"Schweiz"},
+    {city:"København",code:"CPH",country:"Danmark"},
+    {city:"Palma de Mallorca",code:"PMI",country:"Spanien"},
+    {city:"Manchester",code:"MAN",country:"England"},
+    {city:"Oslo",code:"OSL",country:"Norge"},
+    {city:"Lissabon",code:"LIS",country:"Portugal"},
+    {city:"Stockholm",code:"ARN",country:"Sverige"},
+    {city:"London",code:"STN",country:"England"},
+    {city:"Bruxelles",code:"BRU",country:"Belgien"},
+    {city:"Düsseldorf",code:"DUS",country:"Tyskland"},
+    {city:"Wien",code:"VIE",country:"Østrig"},
+    {city:"Milano",code:"MXP",country:"Italien"},
+    {city:"Athen",code:"ATH",country:"Grækenland"},
+    {city:"Berlin",code:"TXL",country:"Tyskland"},
+    {city:"Helsinki",code:"HEL",country:"Finland"},
+    {city:"Málaga",code:"AGP",country:"Spanien"},
+]
+
 const cancelIncurances = [
     200, 300, 500, 1000
 ]
--- SysProjectBackend/src/test/java/facade/DataFacadeTest.java
+++ SysProjectBackend/src/test/java/facade/DataFacadeTest.java
@@ -1,61 +1,69 @@
-//package facade;
-//
-//import entity.Flight;
-//import entity.FlightDTO;
-//import java.util.List;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.Persistence;
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//public class DataFacadeTest {
-//
-//    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("testpu");
-//    private DataFacade facade = new DataFacade();
-//
-//    public DataFacadeTest() {
-//        facade.setEntityManagerFactory(emf);
-//    }
-//
-//    @BeforeClass
-//    public static void setUpClass() {
-//    }
-//
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//
-//    @Before
-//    public void setUp() {
-//    }
-//
-//    @After
-//    public void tearDown() {
-//    }
-//
-//    @org.junit.Test
-//    public void testGetAllFlights() throws Exception {
-//        System.out.println("getAllFlights");
-//        int expResult = 10;
-//        List<FlightDTO> resultList = facade.getAllFlights();
-//        assertEquals(expResult, resultList.size());
-//    }
-//
-//    @org.junit.Test
-//    public void testAddNewFlight() {
-//        System.out.println("addNewFlight");
-//        Flight f = null;
-//        FlightDTO expResult = null;
-//        FlightDTO result = facade.addNewFlight(f);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
+package facade;
+
+import entity.Airport;
+import entity.Flight;
+import entity.FlightDTO;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
+public class DataFacadeTest {
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("testpu");
+    private DataFacade facade = new DataFacade();
+
+    public DataFacadeTest() {
+        facade.setEntityManagerFactory(emf);
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @org.junit.Test
+    public void testAddNewFlightGetAllFlights() throws Exception {
+        System.out.println("addNewFlight & getAllFlights");
+        facade.addNewFlight(new Flight("SAS", new Airport("England", "LHR", "", "London"), new Airport("Frankrig", "CDG", "", "Paris"),
+                "2019-09-01T10:10", "2019-09-01T12:00", 2, 1500, 150, "1", "Boeing 747", 300));
+        facade.addNewFlight(new Flight("Norwegian", new Airport("England", "LHR", "", "London"), new Airport("Holland", "AMS", "", "Amsterdam"),
+                "2019-09-01T10:10", "2019-09-01T12:00", 2, 1500, 150, "1", "Boeing 747", 300));
+        facade.addNewFlight(new Flight("American Airlines", new Airport("England", "LHR", "", "London"), new Airport("Tyskland", "FRA", "", "Frankfurt"),
+                "2019-09-01T10:10", "2019-09-01T12:00", 2, 1500, 150, "1", "Boeing 747", 300));
+        facade.addNewFlight(new Flight("British Airways", new Airport("England", "LHR", "", "London"), new Airport("Tyrkiet", "IST", "", "Istanbul"),
+                "2019-09-01T10:10", "2019-09-01T12:00", 2, 1500, 150, "1", "Boeing 747", 300));
+        facade.addNewFlight(new Flight("Lufthansa", new Airport("England", "LHR", "", "London"), new Airport("Spanien", "MAD", "", "Madrid"),
+                "2019-09-01T10:10", "2019-09-01T12:00", 2, 1500, 150, "1", "Boeing 747", 300));
+        List<FlightDTO> resultList = facade.getAllFlights();
+        assertEquals(5, resultList.size());
+        assertEquals("London, LHR, England", resultList.get(0).getDeparture());
+        assertEquals("London, LHR, England", resultList.get(1).getDeparture());
+        assertEquals("London, LHR, England", resultList.get(2).getDeparture());
+        assertEquals("London, LHR, England", resultList.get(3).getDeparture());
+        assertEquals("London, LHR, England", resultList.get(4).getDeparture());
+        assertEquals("Paris, CDG, Frankrig", resultList.get(0).getDestination());
+        assertEquals("Amsterdam, AMS, Holland", resultList.get(1).getDestination());
+        assertEquals("Frankfurt, FRA, Tyskland", resultList.get(2).getDestination());
+        assertEquals("Istanbul, IST, Tyrkiet", resultList.get(3).getDestination());
+        assertEquals("Madrid, MAD, Spanien", resultList.get(4).getDestination());
+    }
+
 //    @org.junit.Test
 //    public void testEditFlight() {
 //        System.out.println("editFlight");
@@ -66,4 +74,4 @@
 //        // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
 //    }
-//}
+}
