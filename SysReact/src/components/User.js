68f93caf0e36a987735e6becd0b96ea0c0734e48
Stephan D <stephan-mdd@gmail.com>
2018-11-30 14:32:38 +0100

    ordertable fetch

--- SysReact/src/components/User.js
+++ SysReact/src/components/User.js
@@ -5,21 +5,20 @@ import OrderTable from './OrderTable'
 export default class User extends Component{
     constructor(props){
         super(props)
-        this.state = {user:{name:facade.getName(),email:facade.getEmail(),gender:facade.getGender()}}
+        this.state = {user:{name:facade.getName(),email:facade.getEmail(),gender:facade.getGender()}, orders: [], loadingOrders: true}
+        this.getOrders()
     }
 
-    arr = [
-        {depature: '2134', destination: '9876', depTime: '12:00', arrTime: '14:30', amount: '6', price: '1.500', id: 1},
-        {depature: '6803', destination: 'jhga', depTime: '13:00', arrTime: '13:30', amount: '4', price: '26.500', id: 2},
-        {depature: 'jhga', destination: '9876', depTime: '14:00', arrTime: '15:30', amount: '6', price: '5.500', id: 3},
-        {depature: '6bs9', destination: '6bs9', depTime: '16:00', arrTime: '13:30', amount: '2', price: '2.500', id: 4},
-        {depature: 'k8sk', destination: '9876', depTime: '11:00', arrTime: '18:30', amount: '1', price: '6.500', id: 5}
-    ]
+    getOrders = async() => {
+        const orders = await facade.getUserOrders();
+        await this.setState({orders, loadingOrders: false})
+        console.log(this.state.orders)
+    }
 
     render(){
         if (facade.loggedIn() && facade.getRole().includes('user')) {
             return(
-                <div>
+                <div style={{margin: 25}}>
                     <h1>Welcome {facade.getName()}</h1>
                     <div>
                         <p>Username: {this.state.user.name}</p>
@@ -43,7 +42,10 @@ export default class User extends Component{
                         <hr/>
                     </div>
                     <div>
-                        <OrderTable orders={this.arr/*facade.getUserOrders(this.state.username)*/}/>
+                        {(this.state.loadingOrders) 
+                            ? <div style={{fontSize: 24, textAlign: 'center'}}>loading...</div>
+                            : <OrderTable orders={this.state.orders}/>
+                        }
                         </div>
                 </div>
             );
