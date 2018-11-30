80dddca8cedcc4c3314eaa52f7c5f4bcdd523067
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-30 14:28:29 +0100

    delete order endpoint

--- SysProjectBackend/src/main/java/rest/DBOrderResource.java
+++ SysProjectBackend/src/main/java/rest/DBOrderResource.java
@@ -9,13 +9,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import exceptions.InvalidDataException;
 import facade.DataFacade;
+import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,25 +27,30 @@ import javax.ws.rs.core.Response;
  * @author Hupra Laptop
  */
 @Path("order")
-public class DBOrderResource
-{
+public class DBOrderResource {
 
     @Context
     private UriInfo context;
 
+    DataFacade df = new DataFacade();
 
-    public DBOrderResource()
-    {
+    public DBOrderResource() {
+        df.setEntityManagerFactory(Persistence.createEntityManagerFactory("pu"));
     }
 
     @GET
     @Path("id/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getJson(@PathParam("id") int id) throws InvalidDataException
-    {
-        DataFacade df = new DataFacade();
+    public Response getJson(@PathParam("id") int id) throws InvalidDataException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return Response.ok(gson.toJson(df.getOrderByUserId(id))).build();
-        
+    }
+
+    @DELETE
+    @Path("delete/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void deleteOrder(@PathParam("id") int id) {
+        df.deleteOrder(id);
     }
 }
